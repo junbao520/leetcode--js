23e48639fcab4e375adca4f4ae3f2204c0d88e77
junbao_home <757114760@qq.com>
2020-09-13 12:45:20 +0800

    word search dfs 广度优先算法 搜索

---
+++ word-search.js
@@ -0,0 +1,111 @@
+/*给定一个二维网格和一个单词，找出该单词是否存在于网格中。
+
+单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+
+ 
+
+示例:
+
+board =
+[
+  ['A','B','C','E'],
+  ['S','F','C','S'],
+  ['A','D','E','E']
+]
+
+给定 word = "ABCCED", 返回 true
+给定 word = "SEE", 返回 true
+给定 word = "ABCB", 返回 false
+ 
+//考虑分词法
+
+提示：
+
+board 和 word 中只包含大写和小写英文字母。
+1 <= board.length <= 200
+1 <= board[i].length <= 200
+1 <= word.length <= 10^3
+暴击解题思路，递归查询
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/word-search
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+
+
+var exist = function(board, word) {
+    var rLen = board.length,
+        cLen = board[0].length;
+        
+    return helper(word, board, rLen, cLen);
+};
+
+function helper(word, board, rLen, cLen) {
+    var ch = word.charAt(0),
+        i,
+        j;
+        
+    for (i = 0; i < rLen; i++) {
+        for (j = 0; j < cLen; j++) {
+            if (board[i][j] === ch) {
+                board[i][j] = '*';
+                if (bfs(1, word, i, j, board, rLen, cLen)) {
+                    return true;
+                }
+                board[i][j] = ch;
+            }
+        }
+    }
+    
+    return false;
+}
+//bfs 宽度优先搜索
+//dfs 深度优先搜索
+function bfs(index, word, i, j, board, rLen, cLen) {
+    if (index === word.length) {
+        return true;
+    }
+    
+    var ch = word.charAt(index);
+    
+    if (i - 1 >= 0 && board[i - 1][j] === ch) {
+        board[i - 1][j] = '*';
+        
+        if (bfs(index + 1, word, i - 1, j, board, rLen, cLen)) {
+            return true;
+        }
+        
+        board[i - 1][j] = ch;
+    }
+    
+    if (j - 1 >= 0 && board[i][j - 1] === ch) {
+        board[i][j - 1] = '*';
+        
+        if (bfs(index + 1, word, i, j - 1, board, rLen, cLen)) {
+            return true;
+        }
+        
+        board[i][j - 1] = ch;
+    } 
+    
+    if (i + 1 < rLen && board[i + 1][j] === ch) {
+        board[i + 1][j] = '*';
+        
+        if (bfs(index + 1, word, i + 1, j, board, rLen, cLen)) {
+            return true;
+        }
+        
+        board[i + 1][j] = ch;
+    }
+    
+    if (j + 1 < cLen && board[i][j + 1] === ch) {
+        board[i][j + 1] = '*';
+        
+        if (bfs(index + 1, word, i, j + 1, board, rLen, cLen)) {
+            return true;
+        }
+        
+        board[i][j + 1] = ch;
+    }
+    
+}
+
